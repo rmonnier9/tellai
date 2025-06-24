ce20935be2961ca9b0bb49e359d5ae7b807e0c50
Georges Petrov <georgesm.petrov@gmail.com>
2025-06-24 22:30:17 +0200

    wip

--- apps/dashboard/app/dashboard/page.tsx
+++ apps/dashboard/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import { AppSidebar } from '@/components/app-sidebar';
+import { AppSidebar } from "@/components/app-sidebar"
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,18 +6,18 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from '@workspace/ui/components/breadcrumb';
-import { Separator } from '@workspace/ui/components/separator';
+} from "@workspace/ui/components/breadcrumb"
+import { Separator } from "@workspace/ui/components/separator"
 import {
   SidebarInset,
   SidebarProvider,
   SidebarTrigger,
-} from '@workspace/ui/components/sidebar';
+} from "@workspace/ui/components/sidebar"
 
 export default function Page() {
   return (
     <SidebarProvider>
-      <AppSidebar variant="inset" />
+      <AppSidebar />
       <SidebarInset>
         <header className="flex h-16 shrink-0 items-center gap-2">
           <div className="flex items-center gap-2 px-4">
@@ -51,5 +51,5 @@ export default function Page() {
         </div>
       </SidebarInset>
     </SidebarProvider>
-  );
+  )
 }
--- packages/emails/src/index.ts
+++ packages/emails/src/index.ts
@@ -1,3 +1,2 @@
-export * from 'resend';
 export * from './send';
 export * from './magic-link';
