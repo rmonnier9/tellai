90750fcd5f1fa511e8268cacdf2b6e53f157a93f
Georges Petrov <georgesm.petrov@gmail.com>
2025-06-26 20:24:47 +0200

    wip

--- apps/landing/components/waitlist-form.tsx
+++ apps/landing/components/waitlist-form.tsx
@@ -8,19 +8,26 @@ import { toast } from '@workspace/ui/components/sonner';
 
 export default function WaitlistForm() {
   const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
   return (
     <form
-      className="flex lg:flex-row flex-col gap-4"
+      className="flex flex-row gap-2 sm:gap-4 w-full max-w-xl"
       onSubmit={async (e) => {
         e.preventDefault();
+        if (email.length === 0) {
+          return;
+        }
         try {
           setIsLoading(true);
           const formData = new FormData(e.target as HTMLFormElement);
           const email = formData.get('email') as string;
           console.log(email);
           await submitWaitlist(email);
-          toast.success('You are on the waitlist!');
+          toast.success('You are on the waitlist!', {
+            position: 'top-center',
+          });
           (e.target as HTMLFormElement).reset();
+          setEmail('');
         } catch (error) {
           console.error(error);
         } finally {
@@ -30,13 +37,13 @@ export default function WaitlistForm() {
     >
       <Input
         name="email"
-        className="bg-white lg:!text-2xl lg:p-8 rounded-2xl"
+        className="bg-white text-xl p-6 sm:!text-2xl sm:p-8 rounded-2xl flex-1"
         type="email"
         placeholder="Email"
-        disabled={isLoading}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <Button
-        className="lg:text-xl lg:p-8 rounded-2xl"
+        className="text-lg p-6 sm:text-xl sm:p-8 rounded-2xl"
         type="submit"
         disabled={isLoading}
       >
