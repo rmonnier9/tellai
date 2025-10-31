56577927b33b58d47693ce56a936540bd538adcd
Georges Petrov <georgesm.petrov@gmail.com>
2025-10-31 19:25:03 +0100

    /blog

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -10,6 +10,18 @@ const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
+  rewrites: async () => {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://blog.lovarank.com',
+      },
+      {
+        source: '/blog/:path*',
+        destination: 'https://blog.lovarank.com/:path*',
+      },
+    ];
+  },
 };
 
 export default withNextIntl(withMDX(nextConfig));
