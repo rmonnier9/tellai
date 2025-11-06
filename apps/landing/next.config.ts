a90042113e0ad5d89a8f66c2f1652d1a6554c64d
Georges Petrov <georgesm.petrov@gmail.com>
2025-11-06 13:42:01 +0100

    comment rewrites

--- apps/landing/next.config.ts
+++ apps/landing/next.config.ts
@@ -10,28 +10,28 @@ const nextConfig: NextConfig = {
   // Configure `pageExtensions` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
-  rewrites: async () => {
-    return [
-      {
-        source: '/blog',
-        destination: 'https://blog.lovarank.com',
-      },
-      {
-        source: '/blog/:path*',
-        destination: 'https://blog.lovarank.com/:path*',
-      },
-    ];
-  },
-  async headers() {
-    const h = [
-      { key: 'X-Forwarded-Host', value: 'www.lovarank.com' },
-      { key: 'X-Forwarded-Proto', value: 'https' },
-    ];
-    return [
-      { source: '/blog', headers: h },
-      { source: '/blog/:path*', headers: h },
-    ];
-  },
+  // rewrites: async () => {
+  //   return [
+  //     {
+  //       source: '/blog',
+  //       destination: 'https://blog.lovarank.com',
+  //     },
+  //     {
+  //       source: '/blog/:path*',
+  //       destination: 'https://blog.lovarank.com/:path*',
+  //     },
+  //   ];
+  // },
+  // async headers() {
+  //   const h = [
+  //     { key: 'X-Forwarded-Host', value: 'www.lovarank.com' },
+  //     { key: 'X-Forwarded-Proto', value: 'https' },
+  //   ];
+  //   return [
+  //     { source: '/blog', headers: h },
+  //     { source: '/blog/:path*', headers: h },
+  //   ];
+  // },
 };
 
 export default withNextIntl(withMDX(nextConfig));
